5fa680d6e5459d59bc59cac8c476de6fef00b5a3
Angel Fco <angel02fco@gmail.com>
2024-10-04 22:56:52 -0600

    Se agrega el componente Menu

--- src/assets/components/Menu.jsx
+++ src/assets/components/Menu.jsx
@@ -1,3 +1,20 @@
-function Menu() {}
+// src/components/Menu.jsx
+function Menu() {
+  return (
+    <nav>
+      <ul className="menu">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
 
 export default Menu;
