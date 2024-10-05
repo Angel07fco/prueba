a1dca343daa7755bfdab6515d3cf0d6382861f24
Angel Fco <angel02fco@gmail.com>
2024-10-05 09:21:08 -0600

    PRUEB-3 Se añade el contenido de Inicio

--- src/assets/components/Content.jsx
+++ src/assets/components/Content.jsx
@@ -5,16 +5,13 @@ function Content() {
 
   return (
     <div className="card">
-      <h1>Vite + React</h1>
+      <h1>Proyecto de prueba</h1>
       <button onClick={() => setCount((count) => count + 1)}>
         count is {count}
       </button>
-      <p>
-        Edit <code>src/App.jsx</code> and save to test HMR
-      </p>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p></p>
+      <p className="read-the-docs">Ángel Francisco Martínez Hernández</p>
+      <p className="read-the-docs">Abelardo Hernández Hernández</p>
     </div>
   );
 }
